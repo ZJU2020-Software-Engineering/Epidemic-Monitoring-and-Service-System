9811887c8031f056feab3698fbd79678e4431ecf
= <=>
2020-05-06 11:01:09 +0800

    Add OrderList

---
+++ 志愿采购模块/src/orderlist/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import { StyleSheet, Text, View, ScrollView, Alert } from 'react-native';
+// import { Button, List, WhiteSpace, WingBlank } from '@ant-design/react-native';
+import { createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import CurrentOrder from './CurrentOrder';
+import HistoryOrder from './HistoryOrder';
+
+
+const Stack = createStackNavigator();
+export default function App(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="CurrentOrder"
+            >
+                <Stack.Screen
+                    name="CurrentOrder"
+                    component={CurrentOrder}
+                    options={{title: '当前订单列表'}}
+                />
+                <Stack.Screen
+                    name="HistoryOrder"
+                    component={HistoryOrder}
+                    options={{title: '历史订单列表'}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    );
+}
---
+++ 志愿采购模块/src/orderlist/README.md
@@ -0,0 +1,29 @@
+### 订单列表
+
+本目录下为商家端的订单列表。分为当前订单（没有完成配送）列表和历史订单（已完成配送）列表两个页面。当前订单列表中每一项后面有确认打包完成的按钮。
+
+##### 在本项目目录中添加依赖：
+
+```bash
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+```
+
+##### 安装相关库:
+
+```bash
+npm install @react-navigation/native
+npm install @react-navigation/stack
+npm install @ant-design/react-native --save
+```
+或者
+
+```bash
+yarn add @react-navigation/native
+yarn add @react-navigation/stack
+yarn add @ant-design/react-native
+```
+
+##### 说明：
+
+1. 项目包括入口文件App.js以及同一层级的CurrentOrder.js和HistoryOrder.js。
+2. 考虑到和订单类的交互，没有进行数据库操作。
