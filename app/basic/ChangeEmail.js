4c03971a8649d20619281eb4b48c922eed962580
myy565758 <57139047+myy565758@users.noreply.github.com>
2020-06-02 23:00:45 +0800

    Change to Chinese version

--- app/basic/ChangeEmail.js
+++ app/basic/ChangeEmail.js
@@ -32,19 +32,19 @@ export default class ChangeEmail extends React.Component {
         axios.post(ip+'/request/info/personalUserInfo/update',data,{headers:token}) 
         .then((res) => { //alert(JSON.stringify(res.data));
             if(res.data.result=='Y'){
-               alert('Change Success!')
+               alert('修改成功!')
                //this.props.navigation.navigate('Profile',{ username:this.state.username,email:this.state.email } );
                const {navigate,goBack,state} = this.props.navigation;
                 state.params.callback(this.state.email);
                 this.props.navigation.goBack();  
              }
              else if(res.data.result=='L'){
-              alert('token expired,please login again!') 
+              alert('令牌过期，请重新登录!') 
               const { navigate } = this.props.navigation;  
               navigate('Home');
              }
             else
-               alert('Change Failed!') 
+               alert('修改失败!') 
                 })
         .catch((error) => { console.log(error) });
     } 
@@ -57,15 +57,15 @@ export default class ChangeEmail extends React.Component {
       <View style={styles.container}>
       
         <Input
-             placeholder="new email"
-             label='New email'
+             placeholder="新邮箱"
+             label='新邮箱'
              //leftIcon={{ type: 'font-awesome', name: 'address-book' }}
              style={styles}
              onChangeText={value => this.state.email=value}
             /> 
 
            <TouchableOpacity style={styles.button}  onPress={ this.change } >
-              <Text style={styles.buttonText}> submit </Text>
+              <Text style={styles.buttonText}> 提交 </Text>
            </TouchableOpacity> 
 
          
