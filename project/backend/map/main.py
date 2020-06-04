c4452f8ef8f53c3693ef962d94744af67dd5b70e
NWYoung <48687404+NWYoung@users.noreply.github.com>
2020-06-04 21:29:23 +0800

    修改外国数据源

--- project/backend/map/main.py
+++ project/backend/map/main.py
@@ -1,6 +1,17 @@
-from Crawler import Crawler
+from ChinaCrawler import ChinaCrawler
+from ForeignCrawler import ForeignCrawler
+import time
 
 if __name__ == '__main__':
 
-    crawler = Crawler()
-    crawler.run()
+
+    while True:
+        # try:
+        foreign = ForeignCrawler()
+        foreign.crawler()
+        china=ChinaCrawler()
+        china.crawler()
+        # 爬取新数据
+        time.sleep(60 * 30)  # 30分钟更新数据
+    # except BaseException:
+    # continue
