b024d747710d8fb559e0df6678899cdd8382c9bd
NWYoung <48687404+NWYoung@users.noreply.github.com>
2020-06-05 00:31:31 +0800

    Update age.js

--- 疫情地图模块/src/components/dataAnalysis/age.js
+++ 疫情地图模块/src/components/dataAnalysis/age.js
@@ -1,92 +1,108 @@
 import React ,{ Component } from 'react';
 import { View } from 'react-native';
 import { ECharts } from "react-native-echarts-wrapper";
+import {axios} from './App.js'
 
-const data = {
-  china: {
-    countryKey: 'china',
-    countryName: '中国',
-    source: '中华流行病学报告',
-    confirmed: [{
-      name: '0-10',
-      value: 0.9
-    }, {
-      name: '10-20',
-      value: 1.2
-    },
-{
-      name: '20-30',
-      value: 8.1
-    },
-{
-      name: '30-40',
-      value: 17.0
-    },
-    {
-      name: '40-50',
-      value: 19.2
-    },
-{
-      name: '50-60',
-      value: 22.4
-    },
-{
-      name: '60-70',
-      value: 19.2
-    },
-{
-      name: '70-80',
-      value: 8.8
-    },
-{
-      name: '>80',
-      value: 3.2
-    },
-]
-  }
-};
 
+var deathData=[];
+var cureData=[];
 export default class Age extends Component{
+
 	option = {
-	  title: [/*{
-	    text: ``,
-	    top: '15%',
-	    left: '50%',
-	    textAlign: 'center',
-	  }, */
+	  tooltip : {
+	          trigger: 'item',
+	          formatter: "{b} : {c} "
+	      },
+	  title: [
 	  {
-	    subtext: `数据来源:${data.china.source}`,
+	    subtext: `数据来源:中华流行病学报告`,
 	    left: 'right',
-	    top: 0
-	   // textAlign: 'right'
-	  },/* {
-	    subtext: '确诊年龄比',
-	    left: '50%',
-	    top: '50%',
-	    textAlign: 'center'
-	  }*/],
+	    top: 0,
+	  },{
+           subtext: '治愈病例年龄比',
+           left: '50%',
+           top: '45%',
+           textAlign: 'center',
+		   subtextStyle: {
+		   		color: 'black',
+				fontWeight :'bold'
+		   }
+       }, {
+           subtext: '死亡病例年龄比',
+           left: '50%',
+           top: '90%',
+           textAlign: 'center',
+		   subtextStyle: {
+				color: 'black',
+				fontWeight :'bold'
+		   }
+       }],
 	  series: [
-	    {
+	    //治愈病例
+		{
 	      type: 'pie',
-	      radius: '50%',
-	      center: ['50%', '50%'],
-	      data: data.china.confirmed,
-	      animation: false,
+	      radius: '25%',
+	      center: ['50%', '32%'],
+	      data: cureData,
+	      animation: true,
 	      label: {
 	        position: 'outer',
 	        alignTo: 'none',
 	        bleedMargin: 5,
-	        formatter: '{b}: {c}%',
+	        formatter: '{b}',
 	      },
 	      left: 0,
 	      right: '66.6667%',
 	      top: 0,
 	      bottom: 0
-	    }]
+	    },
+		//死亡病例
+		{
+            type: 'pie',
+            radius: '25%',
+            center: ['50%', '80%'],
+            data: deathData,
+            animation: false,
+            label: {
+                position: 'outer',
+                alignTo: 'none',
+                bleedMargin: 5,
+                formatter: '{b}',
+            },
+            left: '66.6667%',
+            right: 0,
+            top: 0,
+            bottom: 0
+           }
+		]
 	};
-	render(){
+	render(){		
+		axios
+		     .post("http://192.168.1.7:8081/request/map/Age/select",{})
+		     .then((res)=>{
+		        var data=res.data.message;
+				var deadSum=0;
+				var cureSum=0;
+				//console.log(data);
+				for(var i=0;i<data.length;i++){
+				   deathData.push({"name":data[i].ageRange+'+',"value":data[i].deathToll});
+				   cureData.push({"name":data[i].ageRange+'+',"value":data[i].cureNumber});
+				   deadSum+=data[i].deathToll;
+				   cureSum+=data[i].cureNumber;
+				}
+				for(var i=0;i<data.length;i++){
+					deathData[i].value=(deathData[i].value/deadSum*100).toFixed(2);
+					cureData[i].value=(cureData[i].value/cureSum*100).toFixed(2);
+				}
+				console.log(deathData);
+				console.log(cureData);
+		    });
+			/*.catch(function (error) {
+			    console.log(error);
+			});*/
+			
 		return(
-		  <View style={{height: 200}}>
+		  <View style={{height: 400}}>
 		      <ECharts
 		         option={this.option}
 		         backgroundColor="white"
