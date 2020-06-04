159bb0f5c8a281e883237993d5a0c40c0d03d041
NWYoung <48687404+NWYoung@users.noreply.github.com>
2020-06-05 00:30:44 +0800

    Update sex.js

--- 疫情地图模块/src/components/dataAnalysis/sex.js
+++ 疫情地图模块/src/components/dataAnalysis/sex.js
@@ -1,162 +1,105 @@
 import React, { Component } from 'react';
 import { View, Dimensions } from 'react-native'
 import { ECharts } from "react-native-echarts-wrapper";
-import { TabView, SceneMap } from 'react-native-tab-view';
-import _ from 'lodash';
+import {axios} from './App.js';
+
+var confirmed=[{name:'男性', value:0 }, 
+	           {name:'女性', value:0 }];
+var deadth=[{name:'男性', value:0 }, 
+	           {name:'女性', value:0 }];
 
 
-export const Sexdata = {
-    china: {
-        countryKey: 'china',
-        countryName: '中国',
-        source: '世界卫生组织-中国联合专家组报告',
-        confirmed: [{
-            name: '男性',
-            value: 51.1
-        }, {
-            name: '女性',
-            value: 48.9
-        }],
-        deceased: [{
-            name: '男性',
-            value: 63.7
-        }, {
-            name: '女性',
-            value: 36.3
-        }]
-    },
-    italy: {
-        countryKey: 'italy',
-        countryName: '意大利',
-        source: '意大利国家卫生院',
-        confirmed: [{
-            name: '男性',
-            value: 58.7
-        }, {
-            name: '女性',
-            value: 41.3
-        }],
-        deceased: [{
-            name: '男性',
-            value: 70.8
-        }, {
-            name: '女性',
-            value: 29.2
-        }]
-    },
-    southKorea: {
-        countryKey: 'southKorea',
-        countryName: '韩国',
-        source: '韩国疾病管理本部',
-        confirmed: [{
-            name: '男性',
-            value: 38.49
-        }, {
-            name: '女性',
-            value: 61.51
-        }],
-        deceased: [{
-            name: '男性',
-            value: 54.26
-        }, {
-            name: '女性',
-            value: 45.74
-        }]
-    }
-};
+export default class Sex extends Component{
 
-export const SexrenderCharts = countryData => {
-   const options = {
-       title: [/*{
-           text: `${countryData.countryName}新冠肺炎病例性别比`,
-           top: '5%',
-           left: '50%',
-           textAlign: 'center',
-       },*/ {
-           subtext: `数据来源：${countryData.source}`,
-           left: '60%',
-           //top: '10%',
-           textAlign: 'center'
-       }, {
-           subtext: '确诊性别比',
-           left: '50%',
-           top: '40%',
-           textAlign: 'center'
-       }, {
-           subtext: '死亡性别比',
-           left: '50%',
-           top: '80%',
-           textAlign: 'center'
-       }],
-       series: [
-           {
-               type: 'pie',
-               radius: '25%',
-               center: ['50%', '30%'],
-               data: countryData.confirmed,
-               animation: false,
-               label: {
-                   position: 'outer',
-                   alignTo: 'none',
-                   bleedMargin: 5,
-                   formatter: '{b}: {c}%',
-               },
-               left: 0,
-               right: '66.6667%',
-               top: 0,
-               bottom: 0
-           },
-           {
-               type: 'pie',
-               radius: '25%',
-               center: ['50%', '70%'],
-               data: countryData.deceased,
-               animation: false,
-               label: {
-                   position: 'outer',
-                   alignTo: 'none',
-                   bleedMargin: 5,
-                   formatter: '{b}: {c}%',
-               },
-               left: '66.6667%',
-               right: 0,
-               top: 0,
-               bottom: 0
-           }
-       ]
-   };
-   
-   return (
-       <View style={{flex: 1}}>
-           <ECharts
-               height={400}
-               option={options}
-               backgroundColor="white"
-           />
-       </View>
-   )
-};
+	options = {
+	    tooltip : {
+	            trigger: 'item',
+	            formatter: "{b} : {c} "
+	        },
+		title: [{
+	        subtext: `数据来源：意大利国家卫生院`,
+	        left: '60%',
+	        //top: '10%',
+	        textAlign: 'center'
+	    }, {
+	        subtext: '确诊病例性别比',
+	        left: '50%',
+	        top: '40%',
+	        textAlign: 'center',
+			subtextStyle: {
+					color: 'black',
+							fontWeight :'bold'
+			}
+	    }, {
+	        subtext: '死亡病例性别比',
+	        left: '50%',
+	        top: '80%',
+	        textAlign: 'center',
+			subtextStyle: {
+					color: 'black',
+							fontWeight :'bold'
+			}
+	    }],
+	    series: [
+	        {
+	            type: 'pie',
+	            radius: '25%',
+	            center: ['50%', '30%'],
+	            data: confirmed,
+	            animation: true,
+	            label: {
+	                position: 'outer',
+	                alignTo: 'none',
+	                bleedMargin: 5,
+	                formatter: '{b}',
+	            },
+	            left: 0,
+	            right: '66.6667%',
+	            top: 0,
+	            bottom: 0
+	        },
+	        {
+	            type: 'pie',
+	            radius: '25%',
+	            center: ['50%', '70%'],
+	            data: deadth,
+	            animation: false,
+	            label: {
+	                position: 'outer',
+	                alignTo: 'none',
+	                bleedMargin: 5,
+	                formatter: '{b}',
+	            },
+	            left: '66.6667%',
+	            right: 0,
+	            top: 0,
+	            bottom: 0
+	        }
+	    ]
+	};
+	
+	render(){		
+		axios
+		     .post("http://192.168.1.7:8081/request/map/Gender/select",{})
+		     .then((res)=>{
+		        var data=res.data.message[0];
+				//console.log(data);
+				confirmed[0].value=data.confirmedRatio;
+				confirmed[1].value=100-confirmed[0].value;
+				deadth[0].value=data.deathRatio;
+				deadth[1].value=100-deadth[0].value;
+				console.log(confirmed);
+				console.log(deadth);
+		    });
 
-export default function SexChart() {
-
-    const initialLayout = { width: Dimensions.get('window').width };
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState(_.map(data, country => ({
-        key: country.countryKey,
-        title: country.countryName,
-    })));
-
-    const renderScene = SceneMap(_.reduce(data, function(map, country) {
-        map[country.countryKey] = () => renderCharts(country);
-        return map;
-    }, {}));
-
-    return (
-        <TabView
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-            initialLayout={initialLayout}
-        />
-    );
+			
+		return(
+		  <View style={{height: 400}}>
+		      <ECharts
+		         option={this.options}
+		         backgroundColor="white"
+		      />
+		  </View>
+		);
+	}
 }
