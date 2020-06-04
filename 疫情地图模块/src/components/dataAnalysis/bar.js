006e76c5e4a3a577e82ba088de9c3df727b17fb9
NWYoung <48687404+NWYoung@users.noreply.github.com>
2020-06-05 00:34:23 +0800

    Update bar.js

--- 疫情地图模块/src/components/dataAnalysis/bar.js
+++ 疫情地图模块/src/components/dataAnalysis/bar.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import { ECharts } from "react-native-echarts-wrapper";
+import {axios} from './App.js'
+import dateTansfer,{year} from './date.js';
 
+var t=new Date();
+var today=dateTansfer(t);
 //获取现存确诊人数最多的五个国家
-var countries=[];
-countries[0]="美国";
-countries[1]="英国";
-countries[2]="意大利";
-countries[3]="中国";
-countries[4]="日本";
-countries[5]="葡萄牙";
-countries[6]="西班牙";
-countries[7]="德国";
-countries[8]="法国";
-countries[9]="韩国";
+export var countries=[];
 
 //获取前十国家新增确诊人数
-var num=[4330,3700,3500,2000,1985,1750,1002,500,200,180];
+var num=[];
 
 export default class WorldBar extends Component {
   option = {
@@ -59,7 +53,23 @@ export default class WorldBar extends Component {
   };
  
   render() {
-    return (
+    var t=new Date();
+    var today=dateTansfer(t);
+	//console.log(today);
+	axios
+         .post("http://192.168.1.7:8081/request/map/foreignMap/select",{"Return":"topTen","Data":today})
+         .then((res)=>{
+            var data=res.data.message;
+    		console.log(data);
+    		for(var i=0;i<data.length;i++){
+				countries[i]=data[i].country;
+				num[i]=data[i].confirmedNumber;
+			}
+			console.log(num);
+			console.log(countries);
+        });
+	
+	return (
       <View style={styles.chartContainer}>
         <ECharts option={this.option} />
       </View>
