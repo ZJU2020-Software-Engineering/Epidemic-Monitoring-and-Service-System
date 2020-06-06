cca212bb3d5338c27831d97e926c98db83192226
myy565758 <57139047+myy565758@users.noreply.github.com>
2020-06-07 03:21:16 +0800

    统一IP

--- app/basic_M/ProfileMerchant.js
+++ app/basic_M/ProfileMerchant.js
@@ -5,7 +5,8 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import { ListItem } from 'react-native-elements'
 var axios = require('axios');
-const ip="http://localhost:8000"
+//const ip="http://localhost:8000"
+var ip=require('./ip')
 export default class ProfileMerchant extends React.Component {
   
 
@@ -29,7 +30,7 @@ export default class ProfileMerchant extends React.Component {
   componentDidMount(){
     //alert(this.state.username)
     var data={
-            'username': this.state.username///'admin'
+            'username': this.state.username
            }
     var token={
           'token':this.state.token,
@@ -66,7 +67,7 @@ export default class ProfileMerchant extends React.Component {
                   tension={100} // These props are passed to the parent component (here TouchableScale)
                   activeScale={0.95} //
                   linearGradientProps={{
-                    colors: ['#a1c3e5', '#1955c9'],
+                    colors: ['#1955c9', '#1955c9'],
                     start: { x: 1, y: 0 },
                     end: { x: 0.2, y: 0 },
                   }}
@@ -76,7 +77,7 @@ export default class ProfileMerchant extends React.Component {
                   titleStyle={{ color: 'white', fontWeight: 'bold' }}
                   subtitleStyle={{ color: 'white' }}
                   subtitle="  用户名"
-                  chevron={{ color: '#796edb' }}
+                  rightIcon={{ type: 'font-awesome', name: 'pencil',color:'white',size:'20'}}
                   //onPress={  () => this.props.navigation.navigate( 'ChangeMerchantName',{username:this.state.username})}
                    onPress={()=>this.props.navigation.navigate('ChangeMerchantName',{
                                 username:this.state.username,token:this.state.token,
@@ -96,8 +97,8 @@ export default class ProfileMerchant extends React.Component {
                   activeScale={0.95} //
                   title={'  电话'}
                   bottomDivider
-                  rightTitle={this.state.phoneNumber}
-                  chevron={true}
+                  rightTitle={this.state.phoneNumber+"  "}
+                  rightIcon={{ type: 'font-awesome', name: 'pencil',size:'20'}}
                   onPress={()=>this.props.navigation.navigate('ChangeMerchantPhonenumber',{
                                 username:this.state.username,token:this.state.token,
                                callback: (data)=>{
@@ -115,8 +116,8 @@ export default class ProfileMerchant extends React.Component {
                   activeScale={0.95} //
                   title={'  邮箱'}
                   bottomDivider
-                  rightTitle={this.state.email}
-                  chevron={true}
+                  rightTitle={this.state.email+" "}
+                  rightIcon={{ type: 'font-awesome', name: 'pencil',size:'20'}}
                   onPress={()=>this.props.navigation.navigate('ChangeMerchantEmail',{
                                 username:this.state.username,token:this.state.token,
                                callback: (data)=>{
@@ -153,8 +154,8 @@ export default class ProfileMerchant extends React.Component {
                   activeScale={0.95} //
                   title={'  经营品类'}
                   bottomDivider
-                  rightTitle={this.state.category}
-                  chevron={true}
+                  rightTitle={this.state.category+"  "}
+                  rightIcon={{ type: 'font-awesome', name: 'pencil',size:'20'}}
                   onPress={()=>this.props.navigation.navigate('ChangeMerchantCategory',{
                                 username:this.state.username,token:this.state.token,
                                callback: (data)=>{
@@ -185,15 +186,15 @@ export default class ProfileMerchant extends React.Component {
                   tension={100} 
                   activeScale={0.95} 
                   linearGradientProps={{
-                    colors: ['#a1c3e5', '#1955c9'],
+                    colors: ['#1955c9', '#1955c9'],
                     start: { x: 1, y: 0 },
                     end: { x: 0.2, y: 0 },
                   }}
                   ViewComponent={LinearGradient} 
                   leftIcon={{ type: 'font-awesome', name: 'lock',color: 'white' }}
-                  title=" 账户与安全"
+                  title=" 修改密码"
                   titleStyle={{ color: 'white', fontWeight: 'bold' }}
-                  chevron={{ color: '#796edb' }}
+                  rightIcon={{ type: 'font-awesome', name: 'pencil',color:'white',size:'20'}}
                   onPress={  () => this.props.navigation.navigate( 'ChangeMerchantPassword',{username:this.state.username,token:this.state.token,})}
               />
              <Text  style={ {fontSize:12}}> </Text>
@@ -203,7 +204,7 @@ export default class ProfileMerchant extends React.Component {
                   tension={100} 
                   activeScale={0.95} 
                   linearGradientProps={{
-                    colors: ['#a1c3e5', '#1955c9'],
+                    colors: ['#1955c9', '#1955c9'],
                     start: { x: 1, y: 0 },
                     end: { x: 0.2, y: 0 },
                   }}
@@ -211,7 +212,7 @@ export default class ProfileMerchant extends React.Component {
                   leftIcon={{ type: 'font-awesome', name: 'tasks',color: 'white' }}
                   title=" 关于"
                   titleStyle={{ color: 'white', fontWeight: 'bold' }}
-                  chevron={{ color: '#796edb' }}
+                  rightIcon={{ type: 'font-awesome', name: 'chevron-right',color:'white',size:'20'}}
                   onPress={  () => this.props.navigation.navigate( 'About',{username:this.state.username,token:this.state.token,})}
               />
              <Text  style={ {fontSize:12}}> </Text>
@@ -221,7 +222,7 @@ export default class ProfileMerchant extends React.Component {
                   tension={100} 
                   activeScale={0.95} 
                   linearGradientProps={{
-                    colors: ['#a1c3e5', '#1955c9'],
+                    colors: ['#1955c9', '#1955c9'],
                     start: { x: 1, y: 0 },
                     end: { x: 0.2, y: 0 },
                   }}
@@ -230,7 +231,7 @@ export default class ProfileMerchant extends React.Component {
                   leftIcon={{ type: 'font-awesome', name: 'question-circle' ,color: 'white'}}
                   title=" 帮助"
                   titleStyle={{ color: 'white', fontWeight: 'bold' }}
-                  chevron={{ color: '#796edb'}}
+                  rightIcon={{ type: 'font-awesome', name: 'chevron-right',color:'white',size:'20'}}
                   onPress={  () => this.props.navigation.navigate( 'Help',{username:this.state.username,token:this.state.token,})}
               />
               <Text  style={ {fontSize:12}}> </Text>
@@ -240,7 +241,7 @@ export default class ProfileMerchant extends React.Component {
                   tension={100} 
                   activeScale={0.95} 
                   linearGradientProps={{
-                    colors: ['#a1c3e5', '#1955c9'],
+                    colors: ['#1955c9', '#1955c9'],
                     start: { x: 1, y: 0 },
                     end: { x: 0.2, y: 0 },
                   }}
@@ -250,7 +251,7 @@ export default class ProfileMerchant extends React.Component {
                   leftIcon={{ type: 'font-awesome', name: 'angle-left',color: 'white' }}
                   title="  注销"
                   titleStyle={{ color: 'white', fontWeight: 'bold' }}
-                  chevron={{ color: '#796edb'}}
+                  //chevron={{ color: '#796edb'}}
                   onPress={  () => this.props.navigation.navigate( 'Home')}
               />
               </View>
