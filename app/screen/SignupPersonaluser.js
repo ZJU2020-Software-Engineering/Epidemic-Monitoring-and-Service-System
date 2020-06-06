f36b273f9a6bfe0eba0a50e92696be0c020e934c
myy565758 <57139047+myy565758@users.noreply.github.com>
2020-06-07 03:18:27 +0800

    统一IP ，优化界面

--- app/screen/SignupPersonaluser.js
+++ app/screen/SignupPersonaluser.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View,ScrollView,TouchableOpacity  } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React , { Component }from 'react';
+import { StyleSheet, Text, View,ScrollView,TouchableOpacity , Picker } from 'react-native';
+
 import { Input,CheckBox,Button } from 'react-native-elements';
 var axios = require('axios');
-const ip="http://localhost:8000"
-
+var ip=require('./ip')
 
 export default class SignupPersonaluser extends React.Component {
 
@@ -13,9 +12,15 @@ export default class SignupPersonaluser extends React.Component {
         this.state = ({ 
           status:'true',
         password:'',
-        confirmedpassword:''  
+        confirmedpassword:'',
+        selected:undefined   
                      })
        
+    } 
+    onValueChange(value: string) {
+      this.setState({
+        selected: value
+      });
     } 
           username ='username';
           //password='';
@@ -26,10 +31,10 @@ export default class SignupPersonaluser extends React.Component {
           phoneNumber='phoneNumber';
           email='email';
           address='address';
-          healthCode='healthCode';
-          healthStatus='healthStatus' ;
+          healthCode='绿码';
+          healthStatus='健康' ;
           gender='gender';
- 
+ ㄎ
   signup= () =>{
 
     if(this.state.status=='true')
@@ -47,7 +52,7 @@ export default class SignupPersonaluser extends React.Component {
         'identityCardNumber': this.identityCardNumber,
         'phoneNumber': this.phoneNumber, 
         'address': this.address, 
-        'healthStatus': this.healthStatus, 
+       'healthStatus': this.healthStatus, 
         'healthCode':this.healthCode,
         //'visitedPlaces':this.visitedPlaces, 
         'email':this.email, 
@@ -66,9 +71,14 @@ export default class SignupPersonaluser extends React.Component {
                                         if(this.healthCode =='绿码' || this.healthCode =='黄码' || this.healthCode =='红码'){
                                           if(this.state.password == this.state.confirmedpassword){
                                             axios.post(ip+'/users/personalUserInfo/insert',data) 
-                                                .then((res) => {  // alert(JSON.stringify(res.data));
-                                            alert("注册成功")
-                                            this.props.navigation.navigate( 'Home')
+                                                .then((res) => {  //alert(JSON.stringify(res.data));
+                                                        if(res.data.result=='N'){
+                                                          alert(res.data.message)
+                                                        }
+                                                        else{
+                                                              alert("注册成功")
+                                                              this.props.navigation.navigate( 'Home')
+                                                        }
                                                                                   })
                                             .catch((error) => {});
                                           }
@@ -147,30 +157,93 @@ export default class SignupPersonaluser extends React.Component {
              style={styles}
              onChangeText={value => this.phoneNumber=value}
         />
-         <Input
-             placeholder="地址"
-             label='地址'
-             style={styles}
-             onChangeText={value => this.address=value}
-        />
+         <Text 
+              style={{alignSelf:'flex-start',marginStart:7,color:'#86939E',fontSize: 16,fontWeight:'bold',textAlign: 'center'}}> 地址</Text>
+        <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              placeholder="地址"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+              style={{width: 120,alignSelf:'flex-start',marginStart:10,marginTop:10}}
+              selectedValue={this.state.selected}
+              onValueChange={this.onValueChange.bind(this),value=>this.address=value}
+            >
+              <Picker.Item label="北京" value="北京" />
+              <Picker.Item label="天津" value="天津" />
+              <Picker.Item label="河北" value="河北" />
+              <Picker.Item label="山西" value="山西" />
+              <Picker.Item label="内蒙古" value="内蒙古" />
+              <Picker.Item label="辽宁" value="辽宁" />
+              <Picker.Item label="吉林" value="吉林" />
+              <Picker.Item label="黑龙江" value="黑龙江" />
+              <Picker.Item label="上海" value="上海" />
+              <Picker.Item label="江苏" value="江苏" />
+              <Picker.Item label="浙江" value="浙江" />
+              <Picker.Item label="安徽" value="安徽" />
+              <Picker.Item label="福建" value="福建" />
+              <Picker.Item label="江西" value="江西" />
+              <Picker.Item label="山东" value="山东" />
+              <Picker.Item label="河南" value="河南" />
+              <Picker.Item label="湖北" value="湖北" />
+              <Picker.Item label="湖南" value="湖南" />
+              <Picker.Item label="广东" value="广东" />
+              <Picker.Item label="广西" value="广西" />
+              <Picker.Item label="海南" value="海南" />
+              <Picker.Item label="重庆" value="重庆" />
+              <Picker.Item label="四川" value="四川" />
+              <Picker.Item label="贵州" value="贵州" />
+              <Picker.Item label="云南" value="云南" />
+              <Picker.Item label="西藏" value="西藏" /> 
+              <Picker.Item label="陕西" value="陕西" />
+              <Picker.Item label="甘肃" value="甘肃" />
+              <Picker.Item label="青海" value="青海" />
+              <Picker.Item label="宁夏" value="宁夏" />
+              <Picker.Item label="新疆" value="新疆" />
+              <Picker.Item label="香港" value="香港" />
+              <Picker.Item label="澳门" value="澳门" />
+              <Picker.Item label="台湾" value="台湾" />
+            </Picker>
          <Input
              placeholder="邮箱"
              label='邮箱'
              style={styles}
              onChangeText={value => this.email=value}
         />
-         <Input
-             placeholder="健康状态"
-             label='健康状态'
-             style={styles}
-             onChangeText={value => this.healthStatus=value}
-        />
-         <Input
-             placeholder="健康码"
-             label='健康码'
-             style={styles}
-             onChangeText={value => this.healthCode=value}
-        />
+         <Text 
+              style={{alignSelf:'flex-start',marginStart:7,color:'#86939E',fontSize: 16,fontWeight:'bold',textAlign: 'center'}}> 健康状态</Text>
+        <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              placeholder="健康状态"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+              style={{width: 120,alignSelf:'flex-start',marginStart:10,marginTop:10}}
+              selectedValue={this.state.selected}
+              onValueChange={this.onValueChange.bind(this),value=>this.healthStatus=value}
+            >
+              <Picker.Item label="健康" value="健康" />
+              <Picker.Item label="疑似" value="疑似" />
+              <Picker.Item label="确诊" value="确诊" />
+              <Picker.Item label="重症" value="重症" />
+              <Picker.Item label="出院" value="出院" />
+            </Picker>
+            <Text 
+              style={{alignSelf:'flex-start',marginStart:7,color:'#86939E',fontSize: 16,fontWeight:'bold',textAlign: 'center',marginTop:10}}> 健康码</Text>
+          <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              placeholder="健康码"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+              style={{width: 120,alignSelf:'flex-start',marginStart:10,marginTop:10}}
+              selectedValue={this.state.selected}
+              onValueChange={this.onValueChange.bind(this),value=>this.healthCode=value}
+            >
+              <Picker.Item label="绿码" value="绿码" />
+              <Picker.Item label="黄码" value="黄码" />
+              <Picker.Item label="红码" value="红码" />
+            </Picker>
         <TouchableOpacity style={styles.button}  onPress={ this.signup} >
               <Text style={styles.buttonText}> 注册 </Text>
         </TouchableOpacity>           
