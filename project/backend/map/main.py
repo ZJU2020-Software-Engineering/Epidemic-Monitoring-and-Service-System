8123bdc94da67ada327f7bbf7d34915219a530e5
NWYoung <48687404+NWYoung@users.noreply.github.com>
2020-06-10 21:33:50 +0800

    Update main.py

--- project/backend/map/main.py
+++ project/backend/map/main.py
@@ -1,17 +1,19 @@
 from ChinaCrawler import ChinaCrawler
 from ForeignCrawler import ForeignCrawler
 import time
+import traceback
 
 if __name__ == '__main__':
 
-
     while True:
-        # try:
-        foreign = ForeignCrawler()
-        foreign.crawler()
-        china=ChinaCrawler()
-        china.crawler()
-        # 爬取新数据
-        time.sleep(60 * 30)  # 30分钟更新数据
-    # except BaseException:
-    # continue
+        try:
+           foreign = ForeignCrawler()
+           foreign.crawler()
+           china=ChinaCrawler()
+           china.crawler()
+           # 爬取新数据
+           time.sleep(60 * 30)  # 30分钟更新数据
+        except BaseException as e:
+           traceback.print_exc()
+           time.sleep(60)  # 休眠一分钟
+           continue
